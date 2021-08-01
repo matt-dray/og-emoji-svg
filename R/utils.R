4f23c1323a935b552b89d6d5b04aadd864899462
Matt Dray <mwdray@gmail.com>
2021-08-01 11:01:57 +0100

    Reorder functions to match usage in script

--- R/utils.R
+++ R/utils.R
@@ -7,31 +7,25 @@ scrape_sb_gif <- function(sb_ext, bow = ep_bow) {
 
 # Function: png to matrix
 pixelate_emoji <- function(path_in) {
-  
   x <- png::readPNG(path_in)
   y <- matrix(as.vector(x), 32)
   t(y[nrow(y):1, 33:64])
-  
+}
+
+# Function: matrix to svg
+save_emoji <- function(emo_mat, file_out, dimn = 10) {
+  svglite::svglite(file_out, width = dimn, height = dimn)
+  par(mar = rep(0, 4), mfrow = c(1, 1))
+  plot_emoji(emo_mat)
+  dev.off()
 }
 
 # Function: matrix to plot
 plot_emoji <- function(emo_mat, col_px = "black", col_bg = "white") {
-  
   image(
     emo_mat,
     col = c(col_bg, col_px),
     useRaster = TRUE,
     axes = FALSE, xaxs = NULL, yaxs = NULL
   )
-  
-}
-
-# Function: matrix to svg
-save_emoji <- function(emo_mat, file_out, dimn = 10) {
-  
-  svglite::svglite(file_out, width = dimn, height = dimn)
-  par(mar = rep(0, 4), mfrow = c(1, 1))
-  plot_emoji(emo_mat)
-  dev.off()
-  
 }
