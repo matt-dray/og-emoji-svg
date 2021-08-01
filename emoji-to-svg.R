6d7e934989c2c0f7346c2267fe3f53dedf868c50
Matt Dray <mwdray@gmail.com>
2021-08-01 11:00:36 +0100

    Add blog link to script, tidy lines a bit

--- emoji-to-svg.R
+++ emoji-to-svg.R
@@ -1,8 +1,11 @@
 # Scrape SoftBank 1997 emoji from Emojipedia and save as SVG
 # Matt Dray, July 2021
 
+# Blog post about this code
+#   https://www.rostrum.blog/2021/07/31/og-emoji-svg/
+
 # SoftBank 1997 hub page on Emojipedia:
-#   https://emojipedia.org/softbank/1997/show_all/#more
+#   https://emojipedia.org/softbank/1997/
 
 # Emojipedia blog about SoftBank as the 'first' emoji set:
 #   https://blog.emojipedia.org/correcting-the-record-on-the-first-emoji-set/
@@ -11,7 +14,7 @@
 # Prepare workspace -------------------------------------------------------
 
 
-# Need a one-time install.packages() if not installed already
+# Use install.packages() if not installed already
 library(polite)
 library(rvest)
 library(purrr)
@@ -27,13 +30,11 @@ source("R/utils.R")  # load bespoke functions
 
 ep_bow <- bow(
   url = "https://emojipedia.org/softbank/1997",
-  user_agent = "M Dray <https://www.matt-dray.com>"
+  user_agent = "M Dray <https://www.matt-dray.com>"  # supply your details
 )
 
 sb_urls <- scrape(ep_bow) |> html_nodes("a") |> html_attr("href")
-
 sb97_urls <- sb_urls[grepl("/1997/", sb_urls) & !grepl("more", sb_urls)]
-
 sb97_img <- map_chr(sb97_urls, scrape_sb_gif)
 
 
@@ -49,23 +50,18 @@ walk2(
 )
 
 
-# Convert gif to PNG ------------------------------------------------------
+# Convert gif to PNG to SVG ------------------------------------------------
 
 
 gif_paths <- list.files("img/gif", pattern = ".gif", full.names = TRUE)
 png_paths <- gsub("gif", "png", gif_paths)
+svg_paths <- gsub("png", "svg", png_paths)
 
 walk2(
   gif_paths,
   png_paths, 
   ~image_read(.x) |> image_write(.y, format = "png")
 )
-
-
-# Create outputs ----------------------------------------------------------
-
-
-svg_paths <- gsub("png", "svg", png_paths)
 
 walk2(
   png_paths, 
@@ -89,8 +85,5 @@ dev.off()
 # Thumbs up, disappointed face, pile of poo (black and white)
 png("plot/emoji-three.png", width = 600, height = 200)
 par(mfrow = c(1, 3), mar = rep(1, 4))
-walk(
-  png_paths[c(82, 19, 55)],
-  ~pixelate_emoji(.x) |> plot_emoji()
-)
+walk(png_paths[c(82, 19, 55)], ~pixelate_emoji(.x) |> plot_emoji())
 dev.off()
